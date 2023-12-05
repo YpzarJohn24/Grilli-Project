ad828b6f65f11cb133f81e8bc8be375fdc9b188a
YpzarJohn24 <ypzarjohn24@gmail.com>
2023-12-05 20:07:49 +0800

    Adding testimonial, features and footer sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,9 +33,10 @@ const toggleNavbar = function () {
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
 
-// HEADER
+// HEADER & BACK TOP BTN
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 let lastScrollPos = 0;
 
@@ -54,9 +55,11 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
     hideHeader();
   } else {
     header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 });
 
